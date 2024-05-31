88b4ee7416c29bcbb85bd338c689f2941d7d1ea5
ArthurCarvalhoTosi <arthurtosi07@hotmail.com>
2024-05-31 09:36:24 -0300

    Jest e Testing Library 3

--- src/components/Posts/Posts.spec.jsx
+++ src/components/Posts/Posts.spec.jsx
@@ -1,4 +1,4 @@
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import { Posts } from ".";
 
 const props = {
@@ -6,14 +6,48 @@ const props = {
         {
             id: 1,
             title: 'title1',
+            body: 'body1',
+            cover: 'img/img1.png',
+        },
+        {
+            id: 2,
+            title: 'title2',
             body: 'body2',
-            cover: 'img/'
-        }
+            cover: 'img/img2.png',
+        },
+        {
+            id: 3,
+            title: 'title3',
+            body: 'body3',
+            cover: 'img/img3.png',
+        },
     ]
 }
 
 describe('Posts/>', () => {
     it('should render posts', () => {
-        render(<Posts />)
+        render(<Posts{...props} />);
+
+        expect(screen.getAllByRole('heading', { name: /title/i }))
+            .toHaveLength(3);
+        expect(screen.getAllByRole('img', { name: /title/i }))
+            .toHaveLength(3);
+        expect(screen.getAllByText(/body/i))
+            .toHaveLength(3);
+        expect(screen.getByRole('img', { name: /title3/i }))
+            .toHaveAttribute('src', 'img/im3.png');
     });
-})
+
+    it('should not render posts ', () => {
+        render(<Posts />);
+        expect(screen.queryAllByRole('heading', { name: /title/i }))
+            .toHaveLength(0);
+    });
+
+
+    it('should match snapshot', () => {
+        const { container } = render(<Posts{...props} />);
+        expect(container.firstChild).toMatchSnapshot();
+    });
+
+});
