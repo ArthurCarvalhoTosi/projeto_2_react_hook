f840f9ae79da2097cf785ed7f36d84649f38a360
Arthur Tosi <arthurtosi07@hotmail.com>
2024-06-02 14:33:21 -0300

    Testando componentes renderizados pela Home

--- src/templates/Home/Home.test.jsx
+++ src/templates/Home/Home.test.jsx
@@ -51,8 +51,19 @@ describe('<Home />', () => {
     render(<Home />);
     const noMorePosts = screen.getByText('Não existem posts');
 
+    expect.assertions(3);
+
     await waitForElementToBeRemoved(noMorePosts);
-    screen.debug();
+
+    const search = screen.getByPlaceholderText(/type your search/i);
+    expect(search).toBeInTheDocument();
+
+    const images = screen.getAllByRole('img', { name: /title/i });
+    expect(images).toHaveLength(2);
+
+    const button = screen.getByRole('button', { name: /Load more posts/i });
+    expect(button).toBeInTheDocument();
+
   });
 });
 
